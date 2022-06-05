1fc2d9649e0df910003110214f030f1fb871a85f
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-05 09:26:16 -0500

    adds httpclientmodule to the app module imports and adds it to the imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -18,6 +19,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule
